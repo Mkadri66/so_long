c540aa5cca3f252d4c4ba9bd20418f77919fe88b
Mustapha Kadri <mkadri@c2r6p8.42perpignan.fr>
2024-03-01 16:41:47 +0100

    edit gitignore file

--- main.c
+++ main.c
@@ -6,21 +6,20 @@
 /*   By: mkadri <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 18:27:51 by mkadri            #+#    #+#             */
-/*   Updated: 2024/03/01 10:46:11 by mkadri           ###   ########.fr       */
+/*   Updated: 2024/03/01 15:25:49 by mkadri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "mlx/mlx.h"
+#include "minilibx/mlx.h"
 #include <stdlib.h>
 #include <stdio.h>
 
 int	main(void)
 {
-	void	*mlx_ptr;
+	void	*mlx;
+	void	*mlx_win;
 
-	mlx_ptr = mlx_init();
-	if (!mlx_ptr)
-		return (10);
-	free(mlx_ptr);
-	return (0);
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
 }
