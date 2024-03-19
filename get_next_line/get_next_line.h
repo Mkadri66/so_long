c7870c3b1f7caa2b67ef43cc61c515086abb9fa6
Mustapha Kadri <mustaphakadri@MacBook-Air-de-Mustapha.local>
2024-03-19 15:17:00 +0100

    readd gnl on stage

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkadri <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/01 20:26:04 by mkadri            #+#    #+#             */
+/*   Updated: 2024/02/23 22:29:16 by mkadri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char *str1, char *str2);
+char	*ft_strchr(const char *str, int searched_char);
+
+#endif
