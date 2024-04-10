4748d7b4e2257b82499821ceb6e24be946301b38
Mustapha Kadri <mustaphakadri@MacBook-Air-de-Mustapha.local>
2024-04-10 02:36:10 +0200

    add printf + norminette

--- libs/game.c
+++ libs/game.c
@@ -1,22 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkadri <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/10 01:57:06 by mkadri            #+#    #+#             */
+/*   Updated: 2024/04/10 02:31:42 by mkadri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "so_long.h"
 
 void	display_player(t_game *game, int height, int width)
 {
-	mlx_put_image_to_window(game->mlx_pointer,game->mlx_win_pointer, game->player, width * 50, height * 50);
+	mlx_put_image_to_window(game->mlx_pointer,
+		game->mlx_win_pointer, game->player, width * 50, height * 50);
 	game->y = height;
 	game->x = width;
 }
 
 void	display_collectable(t_game *game, int height, int width)
 {
-	mlx_put_image_to_window(game->mlx_pointer,game->mlx_win_pointer, game->collectables, width * 50, height * 50);
+	mlx_put_image_to_window(game->mlx_pointer,
+		game->mlx_win_pointer, game->collectables, width * 50, height * 50);
 	game->collectable_count++;
 }
 
 void	set_images_in_map(t_game *game)
 {
-	int	i;
-	int	j;
+	int		i;
+	int		j;
+	char	*mlx;
+
+	mlx = game->mlx_pointer;
 	game->floor = NULL;
 	game->walls = NULL;
 	game->player = NULL;
@@ -24,47 +41,53 @@ void	set_images_in_map(t_game *game)
 	game->collectables = NULL;
 	game->x = 0;
 	game->y = 0;
-	game->floor = mlx_xpm_file_to_image(game->mlx_pointer,"images/floor.xpm", &i, &j);
-	game->walls = mlx_xpm_file_to_image(game->mlx_pointer,"images/walls.xpm", &i, &j);
-	game->player = mlx_xpm_file_to_image(game->mlx_pointer,"images/player.xpm", &i, &j);
-	game->exit = mlx_xpm_file_to_image(game->mlx_pointer,"images/exit.xpm", &i, &j);
-	game->collectables = mlx_xpm_file_to_image(game->mlx_pointer,"images/collectables.xpm", &i, &j);
+	game->floor = mlx_xpm_file_to_image(mlx, "images/floor.xpm", &i, &j);
+	game->walls = mlx_xpm_file_to_image(mlx, "images/walls.xpm", &i, &j);
+	game->player = mlx_xpm_file_to_image(mlx, "images/player.xpm", &i, &j);
+	game->exit = mlx_xpm_file_to_image(mlx, "images/exit.xpm", &i, &j);
+	game->collectables = mlx_xpm_file_to_image(mlx,
+			"images/collectables.xpm", &i, &j);
 }
 
-void display_sprites(t_game *game)
+void	display_sprite(t_game *game, void *sprite, int width, int height)
 {
-    int height;
-    int width;
-
-    game->collectable_count = 0;
-    height = 0;
-    while (height < game->map_height)
-    {
-        width = 0;
-        while (game->map[height][width])
-        {
-            if (game->map[height][width] == '1')
-                mlx_put_image_to_window(game->mlx_pointer,game->mlx_win_pointer, game->walls, width * 50, height * 50);
-            if (game->map[height][width] == '0')
-                mlx_put_image_to_window(game->mlx_pointer,game->mlx_win_pointer, game->floor, width * 50, height * 50);
-            if (game->map[height][width] == 'C')
-                display_collectable(game, height, width);
-            if (game->map[height][width] == 'P')
-                display_player(game, height, width);
-            if (game->map[height][width] == 'E')
-                mlx_put_image_to_window(game->mlx_pointer,game->mlx_win_pointer, game->exit, width * 50, height * 50);
-            width++;
-        }
-        height++;
-    }
+	mlx_put_image_to_window(game->mlx_pointer,
+		game->mlx_win_pointer, sprite, width * 50, height * 50);
 }
 
-void    game_init(t_game *game)
+void	display_sprites(t_game *game)
 {
-    game->mlx_pointer = mlx_init();
-    game->mlx_win_pointer = mlx_new_window(game->mlx_pointer, (game->map_width * 50), (game->map_height * 50), "So_Long");
-    set_images_in_map(game);
-    display_sprites(game);
-    game->movement = 0;
-    //print_game_info(game);
+	int	height;
+	int	width;
+
+	game->collectable_count = 0;
+	height = 0;
+	while (height < game->map_height)
+	{
+		width = 0;
+		while (game->map[height][width])
+		{
+			if (game->map[height][width] == '1')
+				display_sprite(game, game->walls, width, height);
+			if (game->map[height][width] == '0')
+				display_sprite(game, game->floor, width, height);
+			if (game->map[height][width] == 'C')
+				display_collectable(game, height, width);
+			if (game->map[height][width] == 'P')
+				display_player(game, height, width);
+			if (game->map[height][width] == 'E')
+				display_sprite(game, game->exit, width, height);
+			width++;
+		}
+		height++;
+	}
+}
+
+void	game_init(t_game *game)
+{
+	game->mlx_pointer = mlx_init();
+	game->mlx_win_pointer = mlx_new_window(game->mlx_pointer, (game->map_width * 50), (game->map_height * 50), "So_Long");
+	set_images_in_map(game);
+	display_sprites(game);
+	game->movement = 0;
 }
--- libs/game_controls.c
+++ libs/game_controls.c
@@ -3,101 +3,113 @@
 /*                                                        :::      ::::::::   */
 /*   game_controls.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mkadri <mkadri@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mkadri <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 03:42:54 by mkadri            #+#    #+#             */
-/*   Updated: 2024/04/08 00:55:39 by mkadri           ###   ########.fr       */
+/*   Updated: 2024/04/10 02:26:45 by mkadri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 #include <X11/keysym.h>
 
-void move_left(t_game *game)
+void	move_left(t_game *game)
 {
-    if (game->map[game->y][game->x - 1] == '0' || game->map[game->y][game->x - 1] == 'C' || game->map[game->y][game->x - 1] == 'E')
-    {
-        if (game->map[game->y][game->x - 1] == 'C')
-        {
-            game->collectable_count++;
-        }
-		if (game->map[game->y][game->x - 1] == 'E' && game->collectable_count != 0)
-			return;
-		if (game->map[game->y][game->x - 1] == 'E' && game->collectable_count == 0)
-        {
-            exit_game(0, game);
-        }
-        game->map[game->y][game->x] = '0';
-        game->x -= 1;
-        game->map[game->y][game->x] = 'P';
+	char	left_box;
+
+	left_box = game->map[game->y][game->x - 1];
+	if (left_box == '0' || left_box == 'C' || left_box == 'E')
+	{
+		if (left_box == 'C')
+		{
+			game->collectable_count++;
+		}
+		if (left_box == 'E' && game->collectable_count != 0)
+			return ;
+		if (left_box == 'E' && game->collectable_count == 0)
+		{
+			exit_game(0, game);
+		}
+		game->map[game->y][game->x] = '0';
+		game->x -= 1;
+		game->map[game->y][game->x] = 'P';
 		game->movement++;
-    }
+	}
 }
 
-void move_right(t_game *game)
+void	move_right(t_game *game)
 {
-    if (game->map[game->y][game->x + 1] == '0' || game->map[game->y][game->x + 1] == 'C' || game->map[game->y][game->x + 1] == 'E')
-    {
-        if (game->map[game->y][game->x + 1] == 'C')
-        {
-            game->collectable_count++;
-        }
-		if (game->map[game->y][game->x + 1] == 'E' && game->collectable_count != 0)
-			return;
-		if (game->map[game->y][game->x + 1] == 'E' && game->collectable_count == 0)
-        {
-            exit_game(0, game);
-        }
-        game->map[game->y][game->x] = '0';
-        game->x += 1;
-        game->map[game->y][game->x] = 'P';
+	char	right_box;
+
+	right_box = game->map[game->y][game->x + 1];
+	if (right_box == '0' || right_box == 'C' || right_box == 'E')
+	{
+		if (right_box == 'C')
+		{
+			game->collectable_count++;
+		}
+		if (right_box == 'E' && game->collectable_count != 0)
+			return ;
+		if (right_box == 'E' && game->collectable_count == 0)
+		{
+			exit_game(0, game);
+		}
+		game->map[game->y][game->x] = '0';
+		game->x += 1;
+		game->map[game->y][game->x] = 'P';
 		game->movement++;
-    }
+	}
 }
 
-void move_up(t_game *game)
+void	move_up(t_game *game)
 {
-    if (game->map[game->y - 1][game->x] == '0' || game->map[game->y - 1][game->x] == 'C' || game->map[game->y - 1][game->x] == 'E')
-    {
-        if (game->map[game->y - 1][game->x] == 'C')
-        {
-            game->collectable_count++;
-        }
-		if (game->map[game->y - 1][game->x] == 'E' && game->collectable_count != 0)
-			return;
-		if (game->map[game->y - 1][game->x] == 'E' && game->collectable_count == 0)
-        {
-            exit_game(0, game);
-        }
-        game->map[game->y][game->x] = '0';
-        game->y -= 1;
-        game->map[game->y][game->x] = 'P';
+	char	up_box;
+
+	up_box = game->map[game->y - 1][game->x];
+	if (up_box == '0' || up_box == 'C' || up_box == 'E')
+	{
+		if (up_box == 'C')
+		{
+			game->collectable_count++;
+		}
+		if (up_box == 'E' && game->collectable_count != 0)
+			return ;
+		if (up_box == 'E' && game->collectable_count == 0)
+		{
+			exit_game(0, game);
+		}
+		game->map[game->y][game->x] = '0';
+		game->y -= 1;
+		game->map[game->y][game->x] = 'P';
 		game->movement++;
-    }
+	}
 }
 
-void move_down(t_game *game)
+void	move_down(t_game *game)
 {
-    if (game->map[game->y + 1][game->x] == '0' || game->map[game->y + 1][game->x] == 'C' || game->map[game->y + 1][game->x] == 'E')
-    {
-        if (game->map[game->y + 1][game->x] == 'C')
-        {
-            game->collectable_count++;
-        }
-		if (game->map[game->y + 1][game->x] == 'E' && game->collectable_count != 0)
-			return;
-		if (game->map[game->y + 1][game->x] == 'E' && game->collectable_count == 0)
-        {
-            exit_game(0, game);
-        }
-        game->map[game->y][game->x] = '0';
-        game->y += 1;
-        game->map[game->y][game->x] = 'P';
+	char	down_box;
+
+	down_box = game->map[game->y + 1][game->x];
+	if (down_box == '0' || down_box == 'C' || down_box == 'E')
+	{
+		if (down_box == 'C')
+		{
+			game->collectable_count++;
+		}
+		if (down_box == 'E' && game->collectable_count != 0)
+			return ;
+		if (down_box == 'E' && game->collectable_count == 0)
+		{
+			exit_game(0, game);
+		}
+		game->map[game->y][game->x] = '0';
+		game->y += 1;
+		game->map[game->y][game->x] = 'P';
 		game->movement++;
-    }
+	}
 }
 
-int exit_game(int key, t_game *game)
+int	exit_game(int key, t_game *game)
 {
 	int	i;
 
@@ -111,21 +123,21 @@ int exit_game(int key, t_game *game)
 	return (0);
 }
 
-int game_controls(int key, t_game *game)
+int	game_controls(int key, t_game *game)
 {
-    if (key == XK_Escape || key == XK_Cancel)
-        exit_game(key, game);
-    else if (key == XK_Left)
+	if (key == XK_Escape || key == XK_Cancel)
+		exit_game(key, game);
+	else if (key == XK_Left)
 		move_left(game);
-    else if (key == XK_Right)
+	else if (key == XK_Right)
 		move_right(game);
-    else if (key == XK_Up)
+	else if (key == XK_Up)
 		move_up(game);
-    else if (key == XK_Down)
+	else if (key == XK_Down)
 		move_down(game);
-    mlx_clear_window(game->mlx_pointer, game->mlx_win_pointer);
-    display_sprites(game);
-    mlx_do_sync(game->mlx_pointer);
-    printf("Movements : %d  Item : %d \n", game->movement,game->collectable_count);
-    return (0);
+	mlx_clear_window(game->mlx_pointer, game->mlx_win_pointer);
+	display_sprites(game);
+	mlx_do_sync(game->mlx_pointer);
+	printf("Movements : %d \n", game->movement);
+	return (0);
 }
